38f1aa04cc4324c865e95c3aa2688d85e9ffe8b4
coco1986 <46466243+coco1986@users.noreply.github.com>
2022-10-07 17:21:31 -0600

    Delete CS3620 Portfolio/CS3620_Portfolio directory

--- CS3620 Portfolio/CS3620_Portfolio/PortfolioDatabase/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Portfolio
-from .models import Hobbies
-
-# Register your models here.
-
-admin.site.register(Portfolio)
-admin.site.register(Hobbies)
--- CS3620 Portfolio/CS3620_Portfolio/PortfolioDatabase/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('home/', views.home, name='home'),
-    path('hobbies/', views.hobbies, name='hobbies'),
-    path('portfolio/', views.portfolio, name='portfolio'),
-    path('contact/', views.contact, name='contact'),
-]

b2d3530b6e6cc8b717ddf749b1dceeb1104ad440
coco1986 <46466243+coco1986@users.noreply.github.com>
2022-10-28 14:34:15 -0600

    Delete Coding Challenge 7 directory

--- Coding Challenge 7/BookAPI/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import BookData
-
-class BookSerializer(serializers.ModelSerializer):
-    image = serializers.ImageField(max_length=None, use_url=True)
-
-    class Meta:
-        model = BookData
-        fields = ['id', 'name', 'category', 'description', 'rating', 'image']
